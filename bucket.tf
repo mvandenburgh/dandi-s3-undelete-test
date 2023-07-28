7afea35b03dd27344c42c36ff0b65d7f413e1934
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-07-28 11:50:42 -0400

    Add lifecycle policy to expire delete markers

--- bucket.tf
+++ bucket.tf
@@ -33,3 +33,30 @@ data "aws_iam_policy_document" "prevent_deletion_of_object_versions" {
     }
   }
 }
+
+# S3 lifecycle policy that permanently deletes objects with delete markers
+# after 1 day.
+resource "aws_s3_bucket_lifecycle_configuration" "expire_deleted_objects" {
+  # Must have bucket versioning enabled first
+  depends_on = [aws_s3_bucket_versioning.source]
+
+  bucket = aws_s3_bucket.source.id
+
+  # Based on https://docs.aws.amazon.com/AmazonS3/latest/userguide/lifecycle-configuration-examples.html#lifecycle-config-conceptual-ex7
+  rule {
+    id = "ExpireOldDeleteMarkers"
+    filter {}
+
+    # Expire objects with delete markers after 1 day
+    noncurrent_version_expiration {
+      noncurrent_days = 1
+    }
+
+    # Also delete any delete markers associated with the expired object
+    expiration {
+      expired_object_delete_marker = true
+    }
+
+    status = "Enabled"
+  }
+}
