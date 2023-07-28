86c4f8b39b95d53a4f5c72a660a9f02968b88956
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-07-28 11:18:16 -0400

    Add bucket policy preventing deletion of object versions

--- bucket.tf
+++ bucket.tf
@@ -17,3 +17,28 @@ resource "aws_s3_object" "file" {
   source   = "test_files/${each.key}"
   etag     = filemd5("test_files/${each.key}")
 }
+
+resource "aws_s3_bucket_policy" "prevent_deletion_of_object_versions" {
+  bucket = aws_s3_bucket.source.id
+  policy = data.aws_iam_policy_document.prevent_deletion_of_object_versions.json
+}
+
+data "aws_iam_policy_document" "prevent_deletion_of_object_versions" {
+  statement {
+    effect = "Deny"
+
+    actions = [
+      "s3:DeleteObjectVersion",
+    ]
+
+    resources = [
+      aws_s3_bucket.source.arn,
+      "${aws_s3_bucket.source.arn}/*",
+    ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
